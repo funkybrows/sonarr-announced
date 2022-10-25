de03f8854e4e73d7872e117eda620345d94c24ec
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-10-24 23:03:06 -0400

    chore: test torrentleech client

---
+++ test.py
@@ -0,0 +1,22 @@
+import logging
+import sys
+
+from sonarrAnnounced.trackers import Trackers
+from sonarrAnnounced.irc import IRCClient, cfg
+
+
+tl = Trackers().get_tracker("torrentleech")
+
+
+def get_tl_client():
+    client = IRCClient(cfg["torrentleech.nick"])
+    client.set_tracker(tl)
+    return client
+
+
+# client = get_tl_client()
+# client.run(
+#     hostname=tl["irc_host"],
+#     port=tl["irc_port"],
+#     tls=tl["irc_tls"],
+# )
