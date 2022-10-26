a87ee95d1e4c98d5425375a406a170680e272cec
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-10-26 00:54:40 -0400

    feat: get torrent (in bytes) from tlannouncements url

--- sonarrAnnounced/scrapers/tl.py
+++ sonarrAnnounced/scrapers/tl.py
@@ -18,3 +18,15 @@ def login():
         r'class="user_[\w]+">' + cfg["torrentleech.torrent_username"], r.text
     )
     return session
+
+
+def get_torrent_from_url(url):
+    domain = re.search(r"http[s]?://www.torrentleech.[\w]+", url).group(0)
+    session = login()
+    r = session.get("https://www.torrentleech.org/torrent/240921196")
+    torrent_info_match = re.search(r'href=("/[\S]+")>Download[\s]+Torrent', r.text)
+    relative_torrent_url = re.sub(r'href="', "", torrent_info_match.group(0)).split(
+        '"'
+    )[0]
+    r = session.get(f"{domain}{relative_torrent_url}")
+    return r.content
