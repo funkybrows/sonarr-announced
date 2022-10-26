7d9d46aa772cf01f5f45dd003a838b9ba42e9f95
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-10-26 00:27:06 -0400

    test: more realistic message

--- tests/test_sonarr.py
+++ tests/test_sonarr.py
@@ -9,3 +9,12 @@ client = get_sonarr_client()
 
 def test_connect():
     assert client.get_series(1)["id"] == 1
+
+
+def test_parse():
+    msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Tell Me Lies S01E10 720p WEB H264-GLHF' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921295"
+    parsed_title, url = client.get_parsed_tl_announcement(msg)
+    assert parsed_title == "Tell Me Lies S01E10 720p WEB H264-GLHF"
+    assert url == "https://www.torrentleech.org/torrent/240921295"
+
+
