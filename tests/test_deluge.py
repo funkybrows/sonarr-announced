b24a73d177a76c9186c14f6ec1c7cc372b5beea0
Jayson Dorsett <ja_dorsett@jdorsett.com>
2022-10-26 20:26:25 -0400

    test: get_from_url (to be run manually)

--- tests/test_deluge.py
+++ tests/test_deluge.py
@@ -6,7 +6,6 @@ from sonarrAnnounced.scrapers.tl import (
 from sonarrAnnounced.sonarr import get_sonarr_client
 from sonarrAnnounced import config
 import logging
-import requests
 import sys
 
 logger = logging.getLogger(__name__)
@@ -23,3 +22,10 @@ cfg = config.init()
 def test_connect():
     deluge_client.connect_if_necessary()
     assert deluge_client.connected
+
+
+# def test_get_from_url():
+#     deluge_client.connect_if_necessary()
+#     msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Handmade Britains Best Woodworker S02E06 1080p HDTV H264-DARKFLiX' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921997"
+#     parsed_title, url = sonarr_client.get_parsed_tl_announcement(msg)
+#     deluge_client.add_torrent_from_url(parsed_title, url)
