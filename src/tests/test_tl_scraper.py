72c5a0c3b6cc2cc408965fceca8ddd9a84f60f12
Funky Brows <gravity_black@mailbox.org>
2022-11-29 15:36:04 -0500

    test: tl scraping

--- src/tests/test_tl_scraper.py
+++ src/tests/test_tl_scraper.py
@@ -1,5 +1,6 @@
 from requests import Session
-from sonarr_announced.scrapers.tl import login, get_torrent_from_url
+from sonarr_announced.scrapers.tl import get_torrent_data, login
+from sonarr_announced.trackers.tl import get_torrent_id_from_url
 
 
 def test_login():
@@ -7,9 +8,10 @@ def test_login():
     assert isinstance(session, Session)
 
 
-def test_get_torrent_from_url():
+def test_get_torrent_data():
+    name = "Have You Been Paying Attention S10E24 1080p HDTV H264-CBFM"
     url = "https://www.torrentleech.org/torrent/240921196"
-    response_content = get_torrent_from_url(url)
+    response_content = get_torrent_data(name, get_torrent_id_from_url(url))
     assert isinstance(response_content, bytes)
     assert "have.you.been".encode("utf-8") in response_content
     assert "tracker.torrentleech".encode("utf-8") in response_content
