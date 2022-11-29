2a105ac826c7eb293d9cbd3a9587747726a8a741
Funky Brows <gravity_black@mailbox.org>
2022-11-29 15:35:25 -0500

    test: issue in acquiring certain torrent data

--- src/tests/test_deluge.py
+++ src/tests/test_deluge.py
@@ -1,9 +1,13 @@
 from base64 import b64encode
 from sonarr_announced.deluge import get_deluge_client
 from sonarr_announced.scrapers.tl import (
-    get_torrent_from_url as get_tl_torrent_from_url,
+    get_torrent_data as get_tl_torrent_data,
 )
 from sonarr_announced.sonarr import get_sonarr_client
+from sonarr_announced.trackers.tl import (
+    get_name_url_from_msg as get_name_url_from_tl_msg,
+    get_torrent_id_from_url as get_torrent_id_from_tl_url,
+)
 from sonarr_announced import config
 import logging
 import sys
@@ -24,8 +28,9 @@ def test_connect():
     assert deluge_client.connected
 
 
-# def test_get_from_url():
-#     deluge_client.connect_if_necessary()
-#     msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Handmade Britains Best Woodworker S02E06 1080p HDTV H264-DARKFLiX' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921997"
-#     parsed_title, url = sonarr_client.get_parsed_tl_announcement(msg)
-#     deluge_client.add_torrent_from_url(parsed_title, url)
+def test_get_torrent_data():
+    deluge_client.connect_if_necessary()
+    msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Handmade Britains Best Woodworker S02E06 1080p HDTV H264-DARKFLiX' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921997"
+    name, url = get_name_url_from_tl_msg(msg)
+    print(deluge_client.add_torrent(name, get_torrent_id_from_tl_url(url)))
+    stop
--- src/tests/test_sonarr.py
+++ src/tests/test_sonarr.py
@@ -1,5 +1,7 @@
 import sys
 
+from sonarr_announced.trackers.tl import get_name_url_from_msg
+
 print(sys.path)
 import asyncio
 import datetime as dt
@@ -16,22 +18,23 @@ def test_connect():
 
 def test_parse():
     msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Tell Me Lies S01E10 720p WEB H264-GLHF' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921295"
-    parsed_title, url = client.get_parsed_tl_announcement(msg)
+    name, _ = get_name_url_from_msg(msg)
+    parsed_title = client.get_parsed_title_from_sonarr(name)
     assert parsed_title == "Tell Me Lies S01E10 720p WEB H264-GLHF"
-    assert url == "https://www.torrentleech.org/torrent/240921295"
 
 
 def test_push_torrent_release():
     msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <TV :: Episodes HD>  Name:'Tell Me Lies S01E10 720p WEB H264-GLHF' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240921295"
-    parsed_title, url = client.get_parsed_tl_announcement(msg)
+    name, url = get_name_url_from_msg(msg)
+    parsed_title = client.get_parsed_title_from_sonarr(name)
     push_release_info = client.push_torrent_release(parsed_title, url)
     assert push_release_info["rejected"] == True
 
 
 def test_no_elements():
     msg = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <Games :: Nintendo Switch>  Name:'Cubic Figures Update v1 1 0 NSW-SUXXORS' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240922005"
-    parsed_title, url = client.get_parsed_tl_announcement(msg)
-    print(parsed_title, url)
+    name, url = get_name_url_from_msg(msg)
+    parsed_title = client.get_parsed_title_from_sonarr(name)
     push_release_info = client.push_torrent_release(parsed_title, url)
     print(push_release_info)
     stop
