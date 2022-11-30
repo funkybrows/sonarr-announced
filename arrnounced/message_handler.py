a6e2a5f1989c88a7211eeb5bcea49a3610bfc0c1
Funky Brows <gravity_black@mailbox.org>
2022-11-29 20:52:07 -0500

    chore: temporarily mute db usage

--- arrnounced/message_handler.py
+++ arrnounced/message_handler.py
@@ -55,26 +55,14 @@ async def _handle_announcement(tracker, announcement):
         )
         await asyncio.sleep(tracker.config.announce_delay)
 
-    with db.db_session:
-        db_announced = db.insert_announcement(announcement, backends_string)
-    logger.info(
-        "Notifying %s of release from %s: %s",
-        backends_string,
-        tracker.config.short_name,
-        announcement.title,
-    )
+    # with db.db_session:
+    #     db_announced = db.insert_announcement(announcement, backends_string)
 
     backend = await notify(announcement, backends)
 
-    if backend is None:
-        # TODO Print rejection reason
-        logger.debug("Release was rejected: %s", announcement.title)
-    else:
-        announcement.snatched()
-        logger.info("%s approved release: %s", backend.name, announcement.title)
-        with db.db_session:
-            db_announced = db.get_announcement(db_announced.id)
-            db.insert_snatched(db_announced, announcement.snatch_date, backend.name)
+    #     with db.db_session:
+    #         db_announced = db.get_announcement(db_announced.id)
+    #         db.insert_snatched(db_announced, announcement.snatch_date, backend.name)
 
     tracker.status.latest_announcement = announcement
 
