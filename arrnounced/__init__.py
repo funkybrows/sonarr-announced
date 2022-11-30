100419ab6813e6fd30f1e7f79a37fdc92a823811
Funky Brows <gravity_black@mailbox.org>
2022-11-29 20:44:09 -0500

    chore: add logging conf

--- arrnounced/__init__.py
+++ arrnounced/__init__.py
@@ -4,3 +4,12 @@ except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__)
+
+from pathlib import Path
+from os import path
+import logging.config
+
+log_file_path = path.join(
+    Path(__file__).parent.parent.absolute(), "src", "config", "logging.conf"
+)
+logging.config.fileConfig(log_file_path)
