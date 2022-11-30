1a5905e58da8db551bcd3002da53d5596e0c62e9
Funky Brows <gravity_black@mailbox.org>
2022-11-29 21:19:05 -0500

    fix: mock backend notification

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,16 @@
 import pydle
 import pytest
 
+from arrnounced import backend, config
+
 
 @pytest.fixture(scope="session")
 def pydle_pool():
     return pydle.ClientPool()
+
+
+@pytest.fixture(scope="session")
+def user_config():
+    my_user_config = config.init("/config.toml")
+    backend.init(my_user_config.backends)
+    return my_user_config
--- tests/test_tl_announcement.py
+++ tests/test_tl_announcement.py
@@ -1,19 +1,19 @@
 import re
+from unittest import mock
+
 import pytest
-from arrnounced import config
-from arrnounced.announcement import Announcement
 from arrnounced.manager import _get_trackers
 from arrnounced.irc import IRC
 
 
 @pytest.mark.asyncio
-async def test_announcement_generated(pydle_pool):
+async def test_announcement_generated(pydle_pool, user_config):
     message = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <Movies :: BlurayRip>  Name:'Eat Wheaties 2020 BDRip x264-JustWatch' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240943437"
-    tl_tracker = _get_trackers(
-        config.init("/config.toml"), "/autodl-trackers/trackers"
-    )["tl"]
+    tl_tracker = _get_trackers(user_config, "/autodl-trackers/trackers")["tl"]
     tl_irc = IRC(tl_tracker, pydle_pool)
-    announcement = await tl_irc.on_message("#tlannounces", "_AnnounceBot_", message)
+    with mock.patch("arrnounced.message_handler.notify") as mock_notify:
+        mock_notify.return_value = None
+        announcement = await tl_irc.on_message("#tlannounces", "_AnnounceBot_", message)
     assert re.search(
         rf"http[s]?://{tl_tracker.config._xml_config.tracker_info['siteName']}/rss/download/\d+/[a-z0-9]+/Eat\+Wheaties\+2020\+BDRip\+x264-JustWatch",
         announcement.torrent_url,
