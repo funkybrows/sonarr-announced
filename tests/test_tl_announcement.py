d8bf76cfa71a86dc4975f1ed7af25a41ba158cca
Funky Brows <gravity_black@mailbox.org>
2022-12-17 18:23:02 -0500

    chore: untracked code for last commit

--- tests/test_tl_announcement.py
+++ tests/test_tl_announcement.py
@@ -15,7 +15,7 @@ async def test_announcement_generated(
     message = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <Movies :: BlurayRip>  Name:'Eat Wheaties 2020 BDRip x264-JustWatch' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240943437"
     tl_tracker = _get_trackers(user_config, "/autodl-trackers/trackers")["tl"]
     tl_irc = IRC(tl_tracker, pydle_pool)
-        mock_notify.return_value = None
+    mock_notify.return_value = None
     await tl_irc.on_message("#tlannounces", "_AnnounceBot_", message)
     announcement, backends = (
         mock_announce.call_args[0][0],
