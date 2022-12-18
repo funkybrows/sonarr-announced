1d93a828239d7d3fa8e6d648f437ba94d72f0070
Funky Brows <gravity_black@mailbox.org>
2022-12-17 21:56:46 -0500

    chore: lint

--- tests/test_tl_announcement.py
+++ tests/test_tl_announcement.py
@@ -2,6 +2,7 @@ import re
 from unittest import mock
 
 import pytest
+
 from arrnounced.manager import _get_trackers
 from arrnounced.irc import IRC
 
@@ -12,15 +13,12 @@ from arrnounced.irc import IRC
 async def test_announcement_generated(
     mock_notify, mock_announce, pydle_pool, user_config
 ):
-    message = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <Movies :: BlurayRip>  Name:'Eat Wheaties 2020 BDRip x264-JustWatch' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/240943437"
+    message = "\x02\x0300,04New Torrent Announcement:\x02\x0300,12 <Movies :: BlurayRip>  Name:'Eat Wheaties 2020 BDRip x264-JustWatch' uploaded by 'Anonymous' - \x0301,15 https://www.torrentleech.org/torrent/123456789"
     tl_tracker = _get_trackers(user_config, "/autodl-trackers/trackers")["tl"]
     tl_irc = IRC(tl_tracker, pydle_pool)
     mock_notify.return_value = None
     await tl_irc.on_message("#tlannounces", "_AnnounceBot_", message)
-    announcement, backends = (
-        mock_announce.call_args[0][0],
-        mock_announce.call_args[0][1],
-    )
+    announcement = mock_announce.call_args[0][0]
     assert re.search(
         rf"http[s]?://{tl_tracker.config._xml_config.tracker_info['siteName']}/rss/download/\d+/[a-z0-9]+/Eat\+Wheaties\+2020\+BDRip\+x264-JustWatch",
         announcement.torrent_url,
