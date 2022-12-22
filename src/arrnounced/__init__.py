84dffb4fa207b2d70c9a0ea80a60eb4f668c40d9
Funky Brows <gravity_black@mailbox.org>
2022-12-22 16:35:44 -0500

    refactor: get logging.conf and logs folder using get_*_folder_path

--- src/arrnounced/__init__.py
+++ src/arrnounced/__init__.py
@@ -5,22 +5,11 @@ except ModuleNotFoundError:
 
 __version__ = importlib_metadata.version(__name__)
 
-from pathlib import Path
-import os
-from os import path
 import logging.config
 
 from arrnounced import utils
 
-log_file_path = path.join(
-    Path(__file__).parent.parent.parent.absolute(), "config", "logging.conf"
-)
-print(log_file_path)
 logging.config.fileConfig(
-    log_file_path,
-    defaults={
-        "log_folder_path": os.environ.get(
-            "LOG_FOLDER_PATH", utils.get_log_folder_path()
-        )
-    },
+    f"{utils.get_config_folder_path()}/logging.conf",
+    defaults={"log_folder_path": utils.get_log_folder_path()},
 )
