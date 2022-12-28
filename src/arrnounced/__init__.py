52e4d5f822ebd1af252c859bc4edd9284071dafd
Funky Brows <gravity_black@mailbox.org>
2022-12-28 14:10:23 -0500

    fix: not including logging.conf in compose

--- src/arrnounced/__init__.py
+++ src/arrnounced/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 try:
     import importlib.metadata as importlib_metadata
 except ModuleNotFoundError:
@@ -9,7 +11,18 @@ import logging.config
 
 from arrnounced import utils
 
-logging.config.fileConfig(
-    f"{utils.get_config_folder_path()}/logging.conf",
-    defaults={"log_folder_path": utils.get_log_folder_path()},
-)
+logging_conf_path = f"{utils.get_config_folder_path()}/logging.conf"
+log_folder_path = utils.get_log_folder_path()
+
+try:
+    logging.config.fileConfig(
+        logging_conf_path,
+        defaults={"log_folder_path": log_folder_path},
+    )
+except KeyError:
+    base_msg = "COULD NOT SET UP LOGGING"
+    if not os.path.exists(logging_conf_path):
+        print(base_msg, logging_conf_path, "does not exist")
+    if not os.path.exists(log_folder_path):
+        print(base_msg, log_folder_path, "does not exist")
+    raise
